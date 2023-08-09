428de13d6c0e34df7e53e2e6c66aae34686ee28b
Octavio Martinez <omartinez1618@gmail.com>
2023-08-09 01:21:10 -0400

    validate data card agilpay

--- classes/class-agil-payment-method.php
+++ classes/class-agil-payment-method.php
@@ -16,10 +16,46 @@ class AgilPaymentMethod {
     public static function init() {
         require_once 'class-wc-gateway-agil-pay.php';
         add_filter( 'woocommerce_payment_gateways', [__CLASS__, 'add_payment_method']);
+        add_action( 'woocommerce_checkout_process', [__CLASS__, 'validate_agilpay_credentials']);
     }
 
     public static function add_payment_method( $load_gateways ) {
         $load_gateways[] = 'WC_Gateway_AgilPay';
         return $load_gateways;
     }
+
+    public static function validate_agilpay_credentials(){
+        if( isset( $_POST['payment_method'] ) && $_POST['payment_method']=='agilpay' ) {
+            $error_message = "";
+            // agilpay_card_number
+            if(
+                !isset( $_POST['agilpay_card_number'] ) ||
+                $_POST['agilpay_card_number']==''
+            ) {
+                 $error_message .= "Card Number Required! <br/>";
+            } else if ( isset( $_POST['agilpay_card_number'] ) && !is_numeric($_POST['agilpay_card_number']) ) {
+                $error_message .= "Card Number no valid! <br/>";
+            } else if ( isset( $_POST['agilpay_card_number'] ) && strlen($_POST['agilpay_card_number'])!=16 ) {
+                $error_message .= "Card Number length no valid!";
+            }
+            //agilpay_card_expiry
+
+            //agilpay_card_cvv
+            if(
+                !isset( $_POST['agilpay_card_cvv'] ) ||
+                $_POST['agilpay_card_cvv']==''
+            ) {
+                 $error_message .= "CVV Required! <br/>";
+            } else if ( isset( $_POST['agilpay_card_cvv'] ) && !is_numeric($_POST['agilpay_card_cvv']) ) {
+                $error_message .= "CVV no valid! <br/>";
+            } else if ( isset( $_POST['agilpay_card_cvv'] ) && strlen($_POST['agilpay_card_cvv'])!=3 ) {
+                $error_message .= "CVV length no valid!";
+            }
+
+            if( strlen( $error_message )>0 ) {
+                throw new Exception($error_message);
+            }
+        }
+    }
+
 }
