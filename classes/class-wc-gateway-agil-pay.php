ecf3edb7e375e6e8cfa32f80e05a1aded754ac43
mois452b <moisesgabrielrodriguezruiz@gmail.com>
2023-08-31 10:10:34 -0400

    now uses the datas from the form and add validations of payment response

--- classes/class-wc-gateway-agil-pay.php
+++ classes/class-wc-gateway-agil-pay.php
@@ -67,9 +67,33 @@ class WC_Gateway_AgilPay extends WC_Payment_Gateway {
         $order = wc_get_order( $order_id );
 
 		include 'class-agil-pay-request.php';
-		AgilPayRequest::request( );
 
-		// WC()->cart->empty_cart();
+        AgilPayRequest::$merchant_key 	= $this->get_option( 'shop_id' );
+        AgilPayRequest::$client_id 		= $this->get_option( 'client_public_id' );
+        AgilPayRequest::$secret 		= $this->get_option( 'client_secrect_id' );
+		AgilPayRequest::$month 			= explode( '/', $_POST['agilpay_card_expiry'] )[0];
+		AgilPayRequest::$year 			= explode( '/', $_POST['agilpay_card_expiry'] )[1];
+		AgilPayRequest::$cvv 			= $_POST['agilpay_card_cvv'];
+		AgilPayRequest::$card 			= $_POST['agilpay_card_number'];
+		AgilPayRequest::$zipcode 		= $_POST['billing_postcode'];
+		AgilPayRequest::$amount 		= $order->get_total( );
+		AgilPayRequest::$customer_email = $_POST['billing_email'];
+		AgilPayRequest::$customer_name 	= $_POST['billing_first_name'].' '.$_POST['billing_last_name'];
+
+		$payRequest = AgilPayRequest::request( );
+
+		if( $payRequest['ResponseCode'] == '94' ) {
+			return array(
+				'result'   	=> 'error',
+				'message'	=> 'transaccion duplicada'
+			);
+		}
+		if( $payRequest['ResponseCode'] != '00' ) {
+			return array(
+				'result'   => 'error',
+			);
+		}
+		$order->payment_complete();
 		return array(
 			'result'   => 'success',
 			'redirect' => $this->get_return_url( $order ),
